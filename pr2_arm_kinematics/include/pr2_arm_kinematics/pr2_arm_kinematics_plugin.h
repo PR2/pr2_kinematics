a5ccf4703c07b4d4b0dd9e1269bbe6da6e2980be
sachinc <sachinc@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-06-10 02:20:48 +0000

    pluginlib implementation of pr2_arm_kinematics
    git-svn-id: https://code.ros.org/svn/wg-ros-pkg/stacks/pr2_kinematics/trunk@38320 7275ad9f-c29b-430a-bdc5-66f4b3af1622

---
+++ pr2_arm_kinematics/include/pr2_arm_kinematics/pr2_arm_kinematics_plugin.h
@@ -0,0 +1,117 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Sachin Chitta
+*********************************************************************/
+
+#ifndef PR2_ARM_IK_NODE_H
+#define PR2_ARM_IK_NODE_H
+
+#include <ros/ros.h>
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+
+#include <angles/angles.h>
+#include <pr2_arm_kinematics/pr2_arm_ik_solver.h>
+#include <tf_conversions/tf_kdl.h>
+
+#include <kinematics_msgs/GetPositionFK.h>
+#include <kinematics_msgs/GetPositionIK.h>
+#include <kinematics_msgs/GetKinematicSolverInfo.h>
+#include <motion_planning_msgs/ArmNavigationErrorCodes.h>
+
+#include <kdl/chainfksolverpos_recursive.hpp>
+
+#include <boost/shared_ptr.hpp>
+
+#include <kinematics_base/kinematics_base.h>
+
+namespace pr2_arm_kinematics
+{
+class PR2ArmKinematicsPlugin : public kinematics::KinematicsBase
+  {
+    public:
+
+    /** @class
+     *  @brief Plugin-able interface to the PR2 arm kinematics
+     */
+    PR2ArmKinematicsPlugin();
+
+    /** 
+     *  @brief Specifies if the node is active or not
+     *  @return True if the node is active, false otherwise.
+     */
+    bool isActive();
+
+    /** 
+     *  @brief Specifies if the node is active or not
+     *  @return True if the node is active, false otherwise.
+     */
+    bool initialize(std::string name);
+
+    /**
+     * @brief This is the basic IK service method that will compute and return an IK solution.
+     * @param A request message. See service definition for GetPositionIK for more information on this message.
+     * @param The response message. See service definition for GetPositionIK for more information on this message.
+     */
+    bool getPositionIK(kinematics_msgs::GetPositionIK::Request &request, 
+                   kinematics_msgs::GetPositionIK::Response &response);
+
+    /**
+     * @brief This is the basic forward kinematics service that will return information about the kinematics node.
+     * @param A request message. See service definition for GetPositionFK for more information on this message.
+     * @param The response message. See service definition for GetPositionFK for more information on this message.
+     */
+    bool getPositionFK(kinematics_msgs::GetPositionFK::Request &request, 
+                       kinematics_msgs::GetPositionFK::Response &response);
+
+    protected:
+
+    bool active_;
+    int free_angle_;
+    urdf::Model robot_model_;
+    double search_discretization_;
+    ros::NodeHandle node_handle_, root_handle_;
+    boost::shared_ptr<pr2_arm_kinematics::PR2ArmIKSolver> pr2_arm_ik_solver_;
+    ros::ServiceServer ik_service_,fk_service_,ik_solver_info_service_,fk_solver_info_service_;
+    tf::TransformListener tf_;
+    std::string root_name_;
+    int dimension_;
+    boost::shared_ptr<KDL::ChainFkSolverPos_recursive> jnt_to_pose_solver_;
+    KDL::Chain kdl_chain_;
+    kinematics_msgs::KinematicSolverInfo ik_solver_info_, fk_solver_info_;
+  };
+}
+
+#endif
---
+++ pr2_arm_kinematics/src/pr2_arm_kinematics_plugin.cpp
@@ -0,0 +1,243 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Author: Sachin Chitta
+ */
+
+#include <pr2_arm_kinematics/pr2_arm_kinematics_plugin.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <kdl_parser/kdl_parser.hpp>
+#include <tf_conversions/tf_kdl.h>
+#include "ros/ros.h"
+#include <algorithm>
+#include <numeric>
+
+#include <pluginlib/class_list_macros.h>
+
+using namespace KDL;
+using namespace tf;
+using namespace std;
+using namespace ros;
+
+//register PR2ArmKinematics as a KinematicsBase implementation
+PLUGINLIB_DECLARE_CLASS(pr2_arm_kinematics,PR2ArmKinematics, pr2_arm_kinematics::PR2ArmKinematicsPlugin, kinematics::KinematicsBase)
+
+namespace pr2_arm_kinematics {
+
+ PR2ArmKinematicsPlugin::PR2ArmKinematicsPlugin():active_(false){}
+
+  bool PR2ArmKinematicsPlugin::isActive()
+  {
+    if(active_)
+      return true;
+    return false;
+  }
+
+  bool PR2ArmKinematicsPlugin::initialize(std::string name)
+  {
+    urdf::Model robot_model;
+    std::string tip_name, xml_string;
+    ros::NodeHandle private_handle("~/"+name);
+    dimension_ = 7;
+    while(!loadRobotModel(private_handle,robot_model,root_name_,tip_name,xml_string) && private_handle.ok())
+    {
+      ROS_ERROR("Could not load robot model. Are you sure the robot model is on the parameter server?");
+      ros::Duration(0.5).sleep();
+    }
+
+    ROS_INFO("Loading KDL Tree");
+    if(!getKDLChain(xml_string,root_name_,tip_name,kdl_chain_))
+    {
+      active_ = false;
+      ROS_ERROR("Could not load kdl tree");
+    }
+    ROS_INFO("Advertising services");
+    jnt_to_pose_solver_.reset(new KDL::ChainFkSolverPos_recursive(kdl_chain_));
+    private_handle.param<int>("free_angle",free_angle_,2);
+
+    private_handle.param<double>("search_discretization",search_discretization_,0.01);
+    pr2_arm_ik_solver_.reset(new pr2_arm_kinematics::PR2ArmIKSolver(robot_model,root_name_,tip_name, search_discretization_,free_angle_));
+    if(!pr2_arm_ik_solver_->active_)
+    {
+      ROS_ERROR("Could not load ik");
+      active_ = false;
+    }
+    else
+    {
+
+      pr2_arm_ik_solver_->getSolverInfo(ik_solver_info_);
+      pr2_arm_kinematics::getKDLChainInfo(kdl_chain_,fk_solver_info_);
+      fk_solver_info_.joint_names = ik_solver_info_.joint_names;
+
+      for(unsigned int i=0; i < ik_solver_info_.joint_names.size(); i++)
+      {
+        ROS_INFO("PR2Kinematics:: joint name: %s",ik_solver_info_.joint_names[i].c_str());
+      }
+      for(unsigned int i=0; i < ik_solver_info_.link_names.size(); i++)
+      {
+        ROS_INFO("PR2Kinematics can solve IK for %s",ik_solver_info_.link_names[i].c_str());
+      }
+      for(unsigned int i=0; i < fk_solver_info_.link_names.size(); i++)
+      {
+        ROS_INFO("PR2Kinematics can solve FK for %s",fk_solver_info_.link_names[i].c_str());
+      }
+      ROS_INFO("PR2Kinematics::active");
+      active_ = true;
+    }    
+    return active_;
+  }
+
+  bool PR2ArmKinematicsPlugin::getPositionIK(kinematics_msgs::GetPositionIK::Request &request, 
+                                   kinematics_msgs::GetPositionIK::Response &response)
+  {
+    if(!active_)
+    {
+      ROS_ERROR("IK service not active");
+      return true;
+    }
+
+    if(!checkIKService(request,response,ik_solver_info_))
+      return true;
+
+    geometry_msgs::PoseStamped pose_msg_in = request.ik_request.pose_stamped;
+    KDL::Frame pose_desired;
+    if(!convertPoseToRootFrame(pose_msg_in,pose_desired,root_name_,tf_))
+    {
+      response.error_code.val = response.error_code.FRAME_TRANSFORM_FAILURE;
+      return true;
+    }
+
+    //Do the IK
+    KDL::JntArray jnt_pos_in;
+    KDL::JntArray jnt_pos_out;
+    jnt_pos_in.resize(dimension_);
+    for(int i=0; i < dimension_; i++)
+    {
+      int tmp_index = getJointIndex(request.ik_request.ik_seed_state.joint_state.name[i],ik_solver_info_);
+      if(tmp_index >=0)
+      {
+        jnt_pos_in(tmp_index) = request.ik_request.ik_seed_state.joint_state.position[i];
+      }
+      else
+      {
+        ROS_ERROR("i: %d, No joint index for %s",i,request.ik_request.ik_seed_state.joint_state.name[i].c_str());
+      }
+    }
+
+    int ik_valid = pr2_arm_ik_solver_->CartToJntSearch(jnt_pos_in,
+                                                       pose_desired,
+                                                       jnt_pos_out,
+                                                       request.timeout.toSec());
+    if(ik_valid == pr2_arm_kinematics::TIMED_OUT)
+       response.error_code.val = response.error_code.TIMED_OUT;
+    if(ik_valid == pr2_arm_kinematics::NO_IK_SOLUTION)
+       response.error_code.val = response.error_code.NO_IK_SOLUTION;
+
+    if(ik_valid >= 0)
+    {
+      response.solution.joint_state.name = ik_solver_info_.joint_names;
+      response.solution.joint_state.position.resize(dimension_);
+      for(int i=0; i < dimension_; i++)
+      {
+        response.solution.joint_state.position[i] = jnt_pos_out(i);
+        ROS_DEBUG("IK Solution: %s %d: %f",response.solution.joint_state.name[i].c_str(),i,jnt_pos_out(i));
+      }
+      response.error_code.val = response.error_code.SUCCESS;
+      return true;
+    }
+    else
+    {
+      ROS_DEBUG("An IK solution could not be found");   
+      return true;
+    }
+  }
+
+  bool PR2ArmKinematicsPlugin::getPositionFK(kinematics_msgs::GetPositionFK::Request &request, 
+                                   kinematics_msgs::GetPositionFK::Response &response)
+  {
+    if(!active_)
+    {
+      ROS_ERROR("FK service not active");
+      return true;
+    }
+
+    if(!checkFKService(request,response,fk_solver_info_))
+      return true;
+
+    KDL::Frame p_out;
+    KDL::JntArray jnt_pos_in;
+    geometry_msgs::PoseStamped pose;
+    tf::Stamped<tf::Pose> tf_pose;
+
+    jnt_pos_in.resize(dimension_);
+    for(int i=0; i < dimension_; i++)
+    {
+      int tmp_index = getJointIndex(request.robot_state.joint_state.name[i],fk_solver_info_);
+      if(tmp_index >=0)
+        jnt_pos_in(tmp_index) = request.robot_state.joint_state.position[i];
+    }
+
+    response.pose_stamped.resize(request.fk_link_names.size());
+    response.fk_link_names.resize(request.fk_link_names.size());
+
+    bool valid = true;
+    for(unsigned int i=0; i < request.fk_link_names.size(); i++)
+    {
+      ROS_DEBUG("End effector index: %d",pr2_arm_kinematics::getKDLSegmentIndex(kdl_chain_,request.fk_link_names[i]));
+      ROS_DEBUG("Chain indices: %d",kdl_chain_.getNrOfSegments());
+      if(jnt_to_pose_solver_->JntToCart(jnt_pos_in,p_out,pr2_arm_kinematics::getKDLSegmentIndex(kdl_chain_,request.fk_link_names[i])) >=0)
+      {
+        tf_pose.frame_id_ = root_name_;
+        tf_pose.stamp_ = ros::Time();
+        tf::PoseKDLToTF(p_out,tf_pose);
+        try{
+          tf_.transformPose(request.header.frame_id,tf_pose,tf_pose);
+        }
+        catch(...)
+        {
+          ROS_ERROR("Could not transform FK pose to frame: %s",request.header.frame_id.c_str());
+          response.error_code.val = response.error_code.FRAME_TRANSFORM_FAILURE;
+          return false;
+        }
+        tf::poseStampedTFToMsg(tf_pose,pose);
+        response.pose_stamped[i] = pose;
+        response.fk_link_names[i] = request.fk_link_names[i];
+        response.error_code.val = response.error_code.SUCCESS;
+      }
+      else
+      {
+        ROS_ERROR("Could not compute FK for %s",request.fk_link_names[i].c_str());
+        response.error_code.val = response.error_code.NO_FK_SOLUTION;
+        valid = false;
+      }
+    }
+    return true;
+  }
+} // namespace
